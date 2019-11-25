33330bc0b32783ea196d1a284402056819459fe3
GermanRuelas <german.ruelas@tigears.com>
2019-11-25 16:00:14 -0500

    adding address

--- app/Http/Controllers/ApiRest/RegisterController.php
+++ app/Http/Controllers/ApiRest/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\ApiRest;
 
+use App\Address;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\User;
@@ -12,12 +13,20 @@ class RegisterController extends Controller
         $this->validate($request,[
             'email' => 'required|email|unique:users',
             'name' => 'required',
+            'last_name' => 'required',
             'password' => 'required'
         ]);
         $user = User::create([
             'name' => $request->name,
+            'last_name' => $request->last_name,
+            'phone' => $request->phone,
             'email' => $request->email,
             'password' => bcrypt($request->password),
+        ]);
+        $address = Address::create([
+            'alias' => $request->alias,
+            'address' => $request->address,
+            'user_id' => $user->id
         ]);
         return response()->json([
             'message' => "Usuario creado correctamente",
