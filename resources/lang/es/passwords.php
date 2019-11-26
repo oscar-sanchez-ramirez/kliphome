cf1f4799892c8d15b71e0625fc44e0aa2c047ffd
GermanRuelas <german.ruelas@tigears.com>
2019-11-26 10:56:29 -0500

    add spanish version

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'La contrasela debe ser minimamente 6 caracteres y hacer la confirmacion.',
+    'reset' => 'Tu contraseña fue actualizada!',
+    'sent' => '¡Te enviamos un email para actualizar tu contraseña!',
+    'token' => 'This password reset token is invalid.',
+    'user' => "No podemos encontrar al usuario con este correo electrónico",
+
+];
