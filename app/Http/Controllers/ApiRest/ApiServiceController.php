87dbf61779730e22601a22484b73b1225e45eafa
GermanRuelas <german.ruelas@tigears.com>
2019-11-28 17:31:53 -0500

    Update ApiServiceController.php

--- app/Http/Controllers/ApiRest/ApiServiceController.php
+++ app/Http/Controllers/ApiRest/ApiServiceController.php
@@ -21,7 +21,7 @@ class ApiServiceController extends ApiController
         return Response(json_encode(array('services' => $services)));
     }
     public function getCategories(){
-        $categories = Category::all('title');
+        $categories = Category::all('id','title');
         return Response(json_encode(array('categories' => $categories)));
     }
 }
