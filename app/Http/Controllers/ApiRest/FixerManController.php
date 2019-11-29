ab505cf359acb5e22970cccca9afd9c7cf41dec1
GermanRuelas <german.ruelas@tigears.com>
2019-11-29 10:02:21 -0500

    register worker

--- app/Http/Controllers/ApiRest/FixerManController.php
+++ app/Http/Controllers/ApiRest/FixerManController.php
@@ -8,6 +8,30 @@ use Illuminate\Http\Request;
 class FixerManController extends ApiController
 {
     public function register(Request $request){
-        return $request->all();
+        $this->validate($request,[
+            'email' => 'required|email|unique:users',
+            'name' => 'required',
+            // 'lastName' => 'required',
+            'password' => 'required'
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'lastName' => $request->last_name,
+            'phone' => $request->phone,
+            'email' => $request->email,
+            'type' => 'AppFixerMan',
+            'state' => 0,
+            'password' => bcrypt($request->password),
+        ])->toArray();
+        Log::notice($request->all());
+        // Address::create([
+        //     'alias' => $request->alias,
+        //     'address' => $request->address,
+        //     'user_id' => $user["id"]
+        // ]);
+        return response()->json([
+            'message' => "Usuario creado correctamente",
+            'user' => $user
+        ]);
     }
 }
