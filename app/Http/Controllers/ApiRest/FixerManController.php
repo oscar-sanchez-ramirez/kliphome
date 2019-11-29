f26d43c17f3f1a27d5f78bc92d95abb93e5b0dc1
GermanRuelas <german.ruelas@tigears.com>
2019-11-29 12:14:22 -0500

    Update FixerManController.php

--- app/Http/Controllers/ApiRest/FixerManController.php
+++ app/Http/Controllers/ApiRest/FixerManController.php
@@ -12,42 +12,47 @@ use Illuminate\Support\Facades\Log;
 class FixerManController extends ApiController
 {
     public function register(Request $request){
-        Log::info('registrando worker');
-        // $this->validate($request,[
-        //     'email' => 'required|email|unique:users',
-        //     'name' => 'required',
-        //     // 'lastName' => 'required',
-        //     'password' => 'required'
-        // ]);
-        Log::notice($request->all());
-        $user = User::create([
-            'name' => $request->name,
-            'lastName' => $request->last_name,
-            'phone' => $request->phone,
-            'email' => $request->email,
-            'type' => 'AppFixerMan',
-            'state' => 0,
-            'password' => bcrypt($request->password),
-        ])->toArray();
+        try {
+            $this->validate($request,[
+                'email' => 'required|email|unique:users',
+                'name' => 'required',
+                'lastName' => 'required',
+                'password' => 'required'
+            ]);
+            $user = User::create([
+                'name' => $request->name,
+                'lastName' => $request->lastName,
+                'phone' => $request->phone,
+                'email' => $request->email,
+                'type' => 'AppFixerMan',
+                'state' => 0,
+                'password' => bcrypt($request->password),
+            ])->toArray();
 
-        //SAVE SELECTED DELEGATION
-        $selected = new SelectedDelegation;
-        $selected->user_id = $user["id"];
-        $selected->delegation_id = $request->workArea;
-        $selected->save();
+            //SAVE SELECTED DELEGATION
+            $selected = new SelectedDelegation;
+            $selected->user_id = $user["id"];
+            $selected->delegation_id = $request->workArea;
+            $selected->save();
 
-        //SAVE SELECTED CATEGORIES
-        $categories = explode(',',$request->categories);
-        for ($i=0; $i < count($categories); $i++) {
-            $category = new SelectedCategories;
-            $category->user_id = $user["id"];
-            $category->category_id = $categories[$i];
-            $category->save();
+            //SAVE SELECTED CATEGORIES
+            $categories = explode(',',$request->categories);
+            for ($i=0; $i < count($categories); $i++) {
+                $category = new SelectedCategories;
+                $category->user_id = $user["id"];
+                $category->category_id = $categories[$i];
+                $category->save();
+            }
+
+            return response()->json([
+                'message' => "Tu cuenta se creó exitosamente, evaluaremos tu perfil.",
+                'user' => $user
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'fail' => "No se pudo registrar al trabajador, porfavor verifique sus datos"
+            ]);
         }
 
-        return response()->json([
-            'message' => "Usuario creado correctamente",
-            'user' => $user
-        ]);
     }
 }
