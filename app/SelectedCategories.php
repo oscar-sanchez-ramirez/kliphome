ab8f1904ef5fec60cef2da88430852a952756d36
GermanRuelas <german.ruelas@tigears.com>
2019-12-02 18:38:22 -0500

    a

--- app/SelectedCategories.php
+++ app/SelectedCategories.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class SelectedCategories extends Model
 {
+    protected $table = 'selected_categories';
+    protected $primaryKey = 'id';
     public function parent(){
         return $this->belongsTo(Category::class, 'id');
     }
--- app/SelectedDelegation.php
+++ app/SelectedDelegation.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class SelectedDelegation extends Model
 {
+    protected $table = 'selected_delegations';
+    protected $primaryKey = 'id';
     public function parent(){
         return $this->belongsTo(Delegation::class, 'id');
     }
