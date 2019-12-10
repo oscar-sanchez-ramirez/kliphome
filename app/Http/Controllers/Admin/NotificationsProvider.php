c1bdb12397d30f5751738795a5f51a6dbc97dd03
GermanRuelas <german.ruelas@tigears.com>
2019-12-10 14:34:54 -0500

    Update NotificationsProvider.php

--- app/Http/Controllers/Admin/NotificationsProvider.php
+++ app/Http/Controllers/Admin/NotificationsProvider.php
@@ -32,7 +32,7 @@ class NotificationsProvider extends Controller
 
     public function testMatch(){
         $order = Order::where('id',10)->first();
-        $category = $this->table($order->type_service,$rder->selected_id);
+        $category = $this->table($order->type_service,$order->selected_id);
         $user_match_categories = DB::table('users as u')->join('selected_categories as sc','u.id','sc.user_id')->select('u.*')->where('sc.category_id',$category[0]->id)->where('u.state',1)->get();
         foreach ($user_match_categories as $key) {
             $user = User::where('id',$key->id)->first();
