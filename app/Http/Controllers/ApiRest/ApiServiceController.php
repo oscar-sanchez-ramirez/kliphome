e57140f868a3a7036383e65303d100dd31b90764
GermanRuelas <german.ruelas@tigears.com>
2019-12-11 13:00:57 -0500

    Update ApiServiceController.php

--- app/Http/Controllers/ApiRest/ApiServiceController.php
+++ app/Http/Controllers/ApiRest/ApiServiceController.php
@@ -54,7 +54,11 @@ class ApiServiceController extends ApiController
         Log::notice($delegation_id);
         // ->where('o.state','FIXERMAN_NOTIFIED')->orWhere('o.state','PENDING')
         $orders = DB::table('orders as o')->join('addresses as a','o.address','a.id')->where('a.delegation',$delegation_id)
-                ->select('o.*','a.delegation','a.address')->get();
+        ->where(function($query){
+            return $query
+            ->where('o.state','FIXERMAN_NOTIFIED')
+            ->orWhere('o.state','PENDING');
+        })->select('o.*','a.delegation','a.address')->get();
         Log::notice($orders);
         foreach ($orders as $key) {
             $category = $this->table($key->type_service,$key->selected_id);
