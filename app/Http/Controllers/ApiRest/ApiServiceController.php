95fa3a031989f779eec285498fc0a1f1a9103d37
GermanRuelas <german.ruelas@tigears.com>
2019-12-11 12:13:35 -0500

    Update ApiServiceController.php

--- app/Http/Controllers/ApiRest/ApiServiceController.php
+++ app/Http/Controllers/ApiRest/ApiServiceController.php
@@ -54,7 +54,7 @@ class ApiServiceController extends ApiController
         $orders = DB::table('orders')->where('state','FIXERMAN_NOTIFIED')->orWhere('state','PENDING')->get();
         foreach ($orders as $key) {
             $category = $this->table($key->type_service,$key->selected_id);
-            if (in_array($id, $category))
+            if (in_array($ids, $category))
             {
                 $orders.push($category);
             }
