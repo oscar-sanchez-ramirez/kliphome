3282ec2caea982ee266e1e42a37d0296efa5009d
GermanRuelas <german.ruelas@tigears.com>
2019-12-11 10:35:36 -0500

    Update UserLib.php

--- app/Http/Controllers/Admin/Libs/UserLib.php
+++ app/Http/Controllers/Admin/Libs/UserLib.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Http\Controllers\Admin\Libs;
-// use App\User;
+use App\User;
+use App\Address;
 use DB;
+
 
 
 /**
@@ -10,12 +12,12 @@ use DB;
  */
 trait UserLib
 {
-    protected function userInfo($id){
+    public function userInfo($id){
         // return $request->all();
         $user = User::where('id',$id)->first();
         if($user->type == "AppFixerMan"){
-            // $delegation = DB::table('selected_delegations as s')->join('delegations as d','s.delegation_id','d.id')->select('s.id','d.id as delegation_id','d.title')->where('s.user_id',$user->id)->get();
-            // $categories = DB::table('selected_categories as s')->join('categories as c','c.id','s.category_id')->select('s.id','c.id as category_id','c.title')->where('s.user_id',$user->id)->get();
+            $delegation = DB::table('selected_delegations as s')->join('delegations as d','s.delegation_id','d.id')->select('s.id','d.id as delegation_id','d.title')->where('s.user_id',$user->id)->get();
+            $categories = DB::table('selected_categories as s')->join('categories as c','c.id','s.category_id')->select('s.id','c.id as category_id','c.title')->where('s.user_id',$user->id)->get();
             return response()->json([
                 'user' => $user,
                 'delegations' => $delegation,
