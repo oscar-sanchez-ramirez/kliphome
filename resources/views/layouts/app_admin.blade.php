f605d5779c66e35e6fdf065cfb7c6847fb0d460a
GermanRuelas <german.ruelas@tigears.com>
2019-12-12 17:26:37 -0500

    Update app_admin.blade.php

--- resources/views/layouts/app_admin.blade.php
+++ resources/views/layouts/app_admin.blade.php
@@ -332,6 +332,7 @@
                                             <div class="notifi__title">
                                                 <p>You have 3 Notifications</p>
                                             </div>
+                                            @foreach(Auth::user()->unreadNotifications as $notification)
                                             <div class="notifi__item">
                                                 <div class="bg-c1 img-cir img-40">
                                                     <i class="zmdi zmdi-email-open"></i>
@@ -341,24 +342,7 @@
                                                     <span class="date">April 12, 2018 06:50</span>
                                                 </div>
                                             </div>
-                                            <div class="notifi__item">
-                                                <div class="bg-c2 img-cir img-40">
-                                                    <i class="zmdi zmdi-account-box"></i>
-                                                </div>
-                                                <div class="content">
-                                                    <p>Your account has been blocked</p>
-                                                    <span class="date">April 12, 2018 06:50</span>
-                                                </div>
-                                            </div>
-                                            <div class="notifi__item">
-                                                <div class="bg-c3 img-cir img-40">
-                                                    <i class="zmdi zmdi-file-text"></i>
-                                                </div>
-                                                <div class="content">
-                                                    <p>You got a new file</p>
-                                                    <span class="date">April 12, 2018 06:50</span>
-                                                </div>
-                                            </div>
+                                            @endforeach
                                             <div class="notifi__footer">
                                                 <a href="{{ url('') }}/#">All notifications</a>
                                             </div>
