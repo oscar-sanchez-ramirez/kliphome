8ef54db1fa3805158724b0c542c48d35657f1b5a
GermanRuelas <german.ruelas@tigears.com>
2019-12-19 10:17:28 -0500

    Update NotificationsController.php

--- app/Http/Controllers/ApiRest/NotificationsController.php
+++ app/Http/Controllers/ApiRest/NotificationsController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers\ApiRest;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
 use DB;
 
-class NotificationsController extends Controller
+class NotificationsController extends ApiController
 {
     public function getNotifications($id){
         $notifications  = DB::table('notifications')->where('notifiable_id',$id)->get();
