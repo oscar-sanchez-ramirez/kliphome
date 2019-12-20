8ce5c27500d9083502cf3b81996c6e9ea3669566
GermanRuelas <german.ruelas@tigears.com>
2019-12-20 12:03:29 -0500

    Update ClientController.php

--- app/Http/Controllers/ApiRest/ClientController.php
+++ app/Http/Controllers/ApiRest/ClientController.php
@@ -12,9 +12,8 @@ class ClientController extends ApiController
         $orders = DB::table('orders as o')
         ->join('addresses as a','o.address','a.id')
         ->leftJoin('selected_orders as so','o.id','so.order_id')
-        // ->join('users as u','u.id','so.user_id')
-        // ,'u.name','u.lastName'
-        ->select('o.*','a.alias')->where('o.user_id',$id)->get();
+        ->leftJoin('users as u','u.id','so.user_id')
+        ->select('o.*','a.alias','u.name','u.lastName')->where('o.user_id',$id)->get();
         return Response(json_encode(array('orders' => $orders)));
     }
 }
