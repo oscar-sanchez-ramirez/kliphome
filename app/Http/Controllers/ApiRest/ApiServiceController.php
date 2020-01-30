c0282f41b3329ea5b8899a8217148cad81a912e5
GermanRuelas <german.ruelas@tigears.com>
2020-01-30 11:45:07 -0500

    Update ApiServiceController.php

--- app/Http/Controllers/ApiRest/ApiServiceController.php
+++ app/Http/Controllers/ApiRest/ApiServiceController.php
@@ -38,8 +38,9 @@ class ApiServiceController extends ApiController
             Log::debug($delegation);
             $categories = DB::table('selected_categories as s')->join('categories as c','c.id','s.category_id')->select('s.id','c.id as category_id','c.title')->where('s.user_id',$user->id)->get();
             Log::notice($categories);
-            $ids = [];
+            $ids = array();
             foreach($categories as $key) {
+                Log::notice($cat->category_id);
                 array_push($ids,$cat->category_id);
             }
             // array_column(array($categories->toArray()), 'category_id');
