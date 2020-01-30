bded24171f132672598ae2a1506c75b48bee37b9
GermanRuelas <german.ruelas@tigears.com>
2020-01-30 11:01:44 -0500

    Update ApiServiceController.php

--- app/Http/Controllers/ApiRest/ApiServiceController.php
+++ app/Http/Controllers/ApiRest/ApiServiceController.php
@@ -34,7 +34,7 @@ class ApiServiceController extends ApiController
     public function userInfo($id){
         $user = User::where('id',$id)->first();
         if($user->type == "AppFixerMan"){
-            $delegation = DB::table('selected_delegations as s')->join('delegations as d','s.delegation_id','d.id')->select('s.id','d.id as delegation_id','d.title')->where('s.user_id',$user->id)->get();
+            $delegation = DB::table('selected_delegations as s')->join('delegations as d','s.delegation_id','d.id')->select('s.id','d.id as delegation_id','d.title')->where('s.user_id',$user->id)->get()->toArray();
             Log::notice($delegation);
             $categories = DB::table('selected_categories as s')->join('categories as c','c.id','s.category_id')->select('s.id','c.id as category_id','c.title')->where('s.user_id',$user->id)->get()->toArray();
             Log::notice($categories);
