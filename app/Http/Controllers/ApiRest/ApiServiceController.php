7821a1616b926b288df5849f34f9197957c65d7b
GermanRuelas <german.ruelas@tigears.com>
2020-01-30 11:52:29 -0500

    Update ApiServiceController.php

--- app/Http/Controllers/ApiRest/ApiServiceController.php
+++ app/Http/Controllers/ApiRest/ApiServiceController.php
@@ -40,7 +40,8 @@ class ApiServiceController extends ApiController
             Log::notice($categories);
             $ids = [];
             for ($i=0; $i < count($categories); $i++) {
-                $ids[$i] = $categories[$i]->category_id;
+                Log::debug($categories[$i]);
+                $ids[$i] = $categories[$i];
             }
             // array_column(array($categories->toArray()), 'category_id');
             Log::notice($ids);
