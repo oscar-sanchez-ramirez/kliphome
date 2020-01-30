857cde9df8366d061bac7924872c15237e67ff72
GermanRuelas <german.ruelas@tigears.com>
2020-01-30 16:57:06 -0500

    Update ClientController.php

--- app/Http/Controllers/ApiRest/ClientController.php
+++ app/Http/Controllers/ApiRest/ClientController.php
@@ -27,10 +27,10 @@ class ClientController extends ApiController
         foreach ($orders as $key) {
             if($key->state == "FIXERMAN_APPROVED"){
                 $user = DB::table('selected_orders as so')->join('users as u','u.id','so.user_id')
-                ->where('so.state',1)->where('order_id',$key->id)->select('u.*','so.created_at as orderAcepted','so.id as idOrderAccepted')->take(1)->get();
+                ->where('so.state',1)->where('order_id',$key->id)->select('u.*','so.created_at as orderAcepted','so.id as idOrderAccepted')->get();
                 Log::notice($user);
-                Log::notice($user[0]);
-                Log::notice($user[0]->name);
+                Log::notice($user->first());
+
                 $key["name"] = $user[0]->name;
                 $key["lastName"] = $user[0]->lastName;
                 $key["fixerman_id"] = $user[0]->id;
