02aaaa4813bc003f6796624d3ff1898c8fd8b2c7
GermanRuelas <german.ruelas@tigears.com>
2020-02-07 16:03:07 -0500

    fix

--- app/Http/Controllers/ApiRest/ApiServiceController.php
+++ app/Http/Controllers/ApiRest/ApiServiceController.php
@@ -39,7 +39,7 @@ class ApiServiceController extends ApiController
         $user = User::where('id',$id)->first();
 
         if($user->type == "AppFixerMan"){
-            $delegation = DB::table('selected_delegations as s')->join('delegations as d','s.delegation_id','d.id')->select('s.id','d.id as delegation_id','d.title')->where('s.user_id',$user->id)->get();
+            $delegation = DB::table('selected_delegations as s')->where('s.user_id',$user->id)->get();
             $categories = DB::table('selected_categories as s')->join('categories as c','c.id','s.category_id')->select('s.id','c.id as category_id','c.title')->where('s.user_id',$user->id)->get()->toArray();
             $ids = array_column($categories, 'category_id');
             $orders = $this->categories($ids,$delegation[0]->delegation_id,$user->id);
