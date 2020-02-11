22342e6f9053b0f37055cfcc3c4b536c54bedbec
GermanRuelas <german.ruelas@tigears.com>
2020-02-10 19:10:00 -0500

    Update Category.php

--- app/Category.php
+++ app/Category.php
@@ -10,7 +10,7 @@ class Category extends Model
         return SubCategory::where('category_id',$id)->count();
     }
     public function SubCategories($title){
-        return DB::table('categories as c')->join('sub_categories as s','c.id','s.category_id')->select('c.*')->get();
+        return DB::table('categories as c')->join('sub_categories as s','c.id','s.category_id')->select('c.*')->where('c.title',$title)->get();
     }
     protected $hidden = [
         'created_at', 'updated_at',
