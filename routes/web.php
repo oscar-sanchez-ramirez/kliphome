dc9961766afc3a1b81d600b4f36789c77e52a7bf
GermanRuelas <german.ruelas@tigears.com>
2020-02-20 15:49:33 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['register' => false]);
+Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
