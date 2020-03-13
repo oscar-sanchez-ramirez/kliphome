5ab8852a57f587e672ab0735e4fcca0734dbe631
GermanRuelas <german.ruelas@tigears.com>
2020-03-13 17:57:36 -0500

    Update app.php

--- config/app.php
+++ config/app.php
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'America/Merida',
+    'timezone' => 'America/Mexico_City',
 
     /*
     |--------------------------------------------------------------------------
