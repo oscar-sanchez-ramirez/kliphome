8cd50942db6431c648b070548614315b452e9d3f
GermanRuelas <german.ruelas@tigears.com>
2020-03-19 18:02:24 -0500

    Update ordenes.php

--- routes/ordenes.php
+++ routes/ordenes.php
@@ -4,4 +4,4 @@ Route::get('detalle-orden/{id}','Admin\OrderController@orderDetail');
 Route::post('aprobarSolicitudTecnico/{fixerman_id}/{order_id}','Admin\OrderController@aprobarSolicitudTecnico');
 Route::post('eliminarSolicitudTecnico/{fixerman_id}/{order_id}','Admin\OrderController@eliminarSolicitudTecnico');
 Route::post('enviarCotizacion/{order_id}','Admin\OrderController@enviarCotizacion');
-Route::post('notify/{order_id}','Admin\ClientController@notify');
+Route::post('notify/{order_id}','Admin\OrderController@notify');
