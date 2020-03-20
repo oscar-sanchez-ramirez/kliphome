89d60e90c832b552396d4dd84fca8f8ffdeee5f9
GermanRuelas <german.ruelas@tigears.com>
2020-03-19 21:12:59 -0500

    Update tecnicos.php

--- routes/tecnicos.php
+++ routes/tecnicos.php
@@ -2,4 +2,4 @@
 Route::get('/', 'Admin\FixerManController@index');
 Route::get('detalle/{id}','Admin\FixerManController@detail');
 Route::get('aprove','Admin\FixerManController@aprove');
-Route::post('updateFixermanImage','admin\FixerManController@updateFixermanImage');
+Route::post('updateFixermanImage','Admin\FixerManController@updateFixermanImage');
