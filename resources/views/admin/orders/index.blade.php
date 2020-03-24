eb0868e4b1c4d279f9138cf2f99664d9c2198fa8
GermanRuelas <german.ruelas@tigears.com>
2020-03-24 11:35:51 -0500

    Update index.blade.php

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -66,9 +66,9 @@
                                         <td>{{ $orden->created_at->diffForHumans() }}</td>
                                         <td>
                                             <div class="table-data-feature">
-                                                <button class="au-btn au-btn-icon au-btn--green au-btn--small" data-toggle="tooltip" data-placement="top" title="Ver" href="{{ url('') }}/ordenes/detalle-orden/{{ $orden->id }}">
+                                                <a class="au-btn au-btn-icon au-btn--green au-btn--small" data-toggle="tooltip" data-placement="top" title="Ver" href="{{ url('') }}/ordenes/detalle-orden/{{ $orden->id }}">
                                                     Revisar Solicitud
-                                                </button>
+                                                </a>
                                             </div>
                                         </td>
                                     </tr>
