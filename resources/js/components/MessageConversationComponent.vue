af2d32f7de7f1d2fc3017825dd2c0ac5a18f190c
GermanRuelas <german.ruelas@tigears.com>
2020-03-27 11:18:53 -0500

    a

--- resources/js/components/MessageConversationComponent.vue
+++ resources/js/components/MessageConversationComponent.vue
@@ -1,6 +1,6 @@
 <template>
 	<b-media :right-align="!!writtenByMe" vertical-align="center" class="mb-2">
-		<b-img atl="image" class="m-1" rounded="circle"  height="40" :src="image" slot="aside"   width="40"  />
+		<b-img atl="image" class="m-1" rounded="circle"  height="40px" :src="image" slot="aside"   width="40px"  />
 		<b-card><slot></slot></b-card>
 	</b-media>
 </template>
