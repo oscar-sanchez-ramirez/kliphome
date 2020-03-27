d6a13146834d98574d1a5d2e40b055a4e86446a3
GermanRuelas <german.ruelas@tigears.com>
2020-03-27 11:16:41 -0500

    a

--- resources/js/components/ActiveConversationComponent.vue
+++ resources/js/components/ActiveConversationComponent.vue
@@ -6,7 +6,7 @@
 					<p>{{ selectedConversation.contact_name["nombres"]}}</p>
 					<message-conversation-component v-for="message in messages" :key="message.id"
 					:written-by-me="message.written_by_me"
-					:image="message.written_by_me ? myImage : selectedConversation.contact_name['foto']">
+					:image="message.written_by_me ? myImage : selectedConversation.contact_name['avatar']">
 						<div v-if="message.type == 'text'">
 							{{message.content}}
 						</div>
