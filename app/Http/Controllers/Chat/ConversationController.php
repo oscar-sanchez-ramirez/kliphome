2e0ac7bc986e256ca11fbf363f55289f448293b2
GermanRuelas <german.ruelas@tigears.com>
2020-03-27 11:02:49 -0500

    Update ConversationController.php

--- app/Http/Controllers/Chat/ConversationController.php
+++ app/Http/Controllers/Chat/ConversationController.php
@@ -23,7 +23,7 @@ class ConversationController extends ApiController
   public function index()
   {
     return Conversation::get([
-        'id','contact_id','user_id','has_blocked','listen_notifications','last_message','last_time'
+        'id','contact_id','user_id','has_blocked','listen_notifications','last_message','last_time','order_id'
     ]);
 
     // Conversation::where('user_id',Auth::user()->id)->orderBy('last_time',"DESC")->get([
