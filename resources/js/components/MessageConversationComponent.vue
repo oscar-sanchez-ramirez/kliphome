4ca306e88a2c80ab1fb42e476e1e9342fd0cdfe9
GermanRuelas <german.ruelas@tigears.com>
2020-03-27 11:20:38 -0500

    style

--- resources/js/components/MessageConversationComponent.vue
+++ resources/js/components/MessageConversationComponent.vue
@@ -1,9 +1,15 @@
 <template>
 	<b-media :right-align="!!writtenByMe" vertical-align="center" class="mb-2">
-		<b-img atl="image" class="m-1" rounded="circle"  height="40px" :src="image" slot="aside"   width="40px"  />
+		<b-img atl="image" class="m-1" rounded="circle" id="imgUser" :src="image" slot="aside"  />
 		<b-card><slot></slot></b-card>
 	</b-media>
 </template>
+<style>
+	#imgUser{
+		height: 40px;
+		width: 40px;
+	}
+</style>
 <script>
 	export default{
     props:{
