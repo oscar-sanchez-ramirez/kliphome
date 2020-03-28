5afd414d153ea02735c27e0a163c68478a5266e3
GermanRuelas <german.ruelas@tigears.com>
2020-03-27 19:07:50 -0500

    Update ConversationController.php

--- app/Http/Controllers/ApiRest/ConversationController.php
+++ app/Http/Controllers/ApiRest/ConversationController.php
@@ -22,8 +22,9 @@ class ConversationController extends ApiController
   }
 
   public function indexRest($id){
-    return DB::table('conversations as c')->join('users as u','c.contact_id','u.id')->select('c.id','c.contact_id','c.has_blocked','c.listen_notifications','c.last_message','c.last_time','c.order_id','u.name','u.lastName','u.avatar')
-    ->where('user_id',$id)->orWhere('contact_id',$id)->get();
+    return DB::table('conversations as c')->join('users as u','c.contact_id','u.id')
+    ->select('c.id','c.contact_id','c.has_blocked','c.listen_notifications','c.last_message','c.last_time','c.order_id','u.name','u.lastName','u.avatar',DB::raw('IF(c.user_id='.$id.',1,0) as written_by_me'))
+    ->where('c.user_id',$id)->orWhere('c.contact_id',$id)->get();
   }
   public function new_conversation(Request $request){
 
