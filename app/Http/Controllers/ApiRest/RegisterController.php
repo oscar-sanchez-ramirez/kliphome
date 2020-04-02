ee92c1ad732c526790c20944cf9d3779ba4da73a
GermanRuelas <german.ruelas@tigears.com>
2020-04-02 00:22:18 -0500

    Update RegisterController.php

--- app/Http/Controllers/ApiRest/RegisterController.php
+++ app/Http/Controllers/ApiRest/RegisterController.php
@@ -30,12 +30,12 @@ class RegisterController extends ApiController
             'street' => $request->street,
             'alias' => $request->alias,
             'reference' => $request->reference,
-            'postal_code' => $request->postal_code,
+            'postal_code' => "-",
             'user_id' => $user["id"],
             'delegation' => "-",
             'exterior' => $request->exterior,
             'interior' => $request->interior,
-            'colonia' => $request->colonia,
+            'colonia' => "-",
             'municipio' => $request->municipio
         ]);
         return response()->json([
