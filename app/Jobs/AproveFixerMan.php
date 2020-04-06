5514d08484a25c006ee35f71e868816f18d0cd79
GermanRuelas <german.ruelas@tigears.com>
2020-04-06 13:02:51 -0500

    Update AproveFixerMan.php

--- app/Jobs/AproveFixerMan.php
+++ app/Jobs/AproveFixerMan.php
@@ -35,7 +35,7 @@ class AproveFixerMan implements ShouldQueue
         $user->mensajeFixerMan = "Tu cuenta fue aprobada";
         $user->notify(new ApproveFixerMan($user));
         $notification = $user->notifications()->first();
-        // $user->created_at = $notification->id;
+        $user->notification_id = $notification->id;
         $user->sendNotification($user->email,'ApproveFixerMan',$user);
     }
 }
