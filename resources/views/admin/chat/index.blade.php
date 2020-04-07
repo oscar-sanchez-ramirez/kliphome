a34af92e18a58eddfc93c294944b0b7b252a73de
GermanRuelas <german.ruelas@tigears.com>
2020-04-06 20:58:16 -0500

    a

--- resources/views/admin/chat/index.blade.php
+++ resources/views/admin/chat/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="main-content">
         <div class="section__content section__content--p30">
-            <div class="card">
+            <div class="card" id="cardContent">
                 <messenger-component :user="{{ auth()->user() }}" :order="{{ intval($order) }}"></messenger-component>
             </div>
         </div>
