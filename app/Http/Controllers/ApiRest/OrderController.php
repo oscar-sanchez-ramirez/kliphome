fc205b13bda10d12b990f0da01f6de7976dc03ef
GermanRuelas <german.ruelas@tigears.com>
2020-04-08 22:37:57 -0500

    Update OrderController.php

--- app/Http/Controllers/ApiRest/OrderController.php
+++ app/Http/Controllers/ApiRest/OrderController.php
@@ -26,7 +26,7 @@ class OrderController extends ApiController
         // try {
             // $price = 'quotation';
             // $price = floatval($request->price);
-            try {
+            // try {
                 Stripe\Stripe::setApiKey("sk_test_f2VYH7q0KzFbrTeZfSvSsE8R00VBDQGTPN");
                 if(substr($request->token,0,3) == "cus"){
                     $pago = Stripe\Charge::create ([
@@ -70,17 +70,17 @@ class OrderController extends ApiController
                     'success' => true,
                     'message' => "La orden de servicio se realizó con éxito"
                 ]);
-            } catch (\Throwable $th) {
-                $payment = new Payment;
-                $payment->order_id = $order->id;
-                $payment->description = "VISITA";
-                $payment->state = false;
-                $payment->price = $request->visit_price;
-                $payment->save();
-                return response()->json([
-                    'success' => false
-                ]);
-            }
+            // } catch (\Throwable $th) {
+            //     $payment = new Payment;
+            //     $payment->order_id = $order->id;
+            //     $payment->description = "VISITA";
+            //     $payment->state = false;
+            //     $payment->price = $request->visit_price;
+            //     $payment->save();
+            //     return response()->json([
+            //         'success' => false
+            //     ]);
+            // }
 
         // } catch (\Throwable $th) {
         //     return response()->json([
