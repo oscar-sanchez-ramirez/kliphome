d0e3edb980cf37e18c00a6d8f652c47a0420a77c
GermanRuelas <german.ruelas@tigears.com>
2020-04-13 18:28:19 -0500

    Update index.blade.php

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -46,7 +46,7 @@
                                         <td>{{ $orden->created_at->diffForHumans() }}</td>
                                         <td>
                                             <div class="table-data-feature">
-                                                @if($order->state == "CANCELLED")
+                                                @if($orden->state == "CANCELLED")
                                                 <span class="status--denied">Cancelado</span>
                                                 @else
                                                     <a class="au-btn au-btn-icon au-btn--green au-btn--small" data-toggle="tooltip" data-placement="top" title="Ver" href="{{ url('') }}/ordenes/detalle-orden/{{ $orden->id }}">
