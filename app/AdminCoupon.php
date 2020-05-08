8022d0fbd02584fbf986518aaec7ac467fabe02f
GermanRuelasCastillo <spartperu@gmail.com>
2020-05-08 12:20:27 -0500

    Update AdminCoupon.php

--- app/AdminCoupon.php
+++ app/AdminCoupon.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class AdminCoupon extends Model
 {
     public function getUser($id){
-        User::where('id',$id)->first(['name','lastName']);
+        return User::where('id',$id)->first(['name','lastName']);
     }
 }
