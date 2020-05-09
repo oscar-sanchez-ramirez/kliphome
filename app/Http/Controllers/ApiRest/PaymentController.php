689499fe62553036a70d49570c03c30b85fabcfe
GermanRuelasCastillo <spartperu@gmail.com>
2020-05-09 11:36:31 -0500

    Update PaymentController.php

--- app/Http/Controllers/ApiRest/PaymentController.php
+++ app/Http/Controllers/ApiRest/PaymentController.php
@@ -15,12 +15,14 @@ class PaymentController extends ApiController
     }
     public function saveCustomer(Request $request)
     {
+        Log::notice($request->all());
         $user = $request->user();
         Stripe\Stripe::setApiKey("sk_test_f2VYH7q0KzFbrTeZfSvSsE8R00VBDQGTPN");
         $customer = Stripe\Customer::create ([
             "source" => $request->stripeToken,
             "description" => "Card of".$user->name.' '.$user->lastName
         ]);
+        Log::notice($customer);
         return response()->json([
             'success' => true,
             'card' => $customer
