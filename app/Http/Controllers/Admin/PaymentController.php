7c2ee554b4ec21910a53c2cc68a58be14bf1b622
GermanRuelasCastillo <spartperu@gmail.com>
2020-06-13 23:08:01 -0500

    Update PaymentController.php

--- app/Http/Controllers/Admin/PaymentController.php
+++ app/Http/Controllers/Admin/PaymentController.php
@@ -21,7 +21,7 @@ class PaymentController extends Controller
 
     public function index(){
         $general_percent = DB::table('general_stats')->where('title',"percent")->first();
-        $payments = DB::table('selected_orders as so')->join('users as u','u.id','so.user_id')->join('fixerman_stats as ft','ft.user_id','u.id')->join('orders as o','o.id','so.order_id')->leftJoin('quotations as q','o.id','q.order_id')->join('payments as p','p.order_id','o.id')
+        $payments = DB::table('orders as o')->leftJoin('selected_orders as so','o.id','so.order_id')->join('users as u','u.id','so.user_id')->join('fixerman_stats as ft','ft.user_id','u.id')->leftJoin('quotations as q','o.id','q.order_id')->join('payments as p','p.order_id','o.id')
         ->select('p.*','q.workforce','q.price as service_price','ft.percent','u.name','u.lastName')->orderBy('p.id',"DESC")->get();
         return view('admin.payments.index')->with('payments',$payments)->with('general_percent',$general_percent);
     }
