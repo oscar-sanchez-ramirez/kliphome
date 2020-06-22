861d3832eac1521b1d50cc668533195db3e91a3f
GermanRuelasCastillo <spartperu@gmail.com>
2020-06-22 17:25:52 -0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,15 +11,15 @@
   <title>KlipHome</title>
 
   <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="{{ url('') }}/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom fonts for this template -->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="{{ url('') }}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   <link rel="shortcut icon" type="image/x-icon" href="{{url('')}}/img/favicon.ico">
   <!-- Custom styles for this template -->
-  <link href="css/grayscale.min.css" rel="stylesheet">
+  <link href="{{ url('') }}/css/grayscale.min.css" rel="stylesheet">
 
 </head>
 <body id="page-top">
@@ -39,14 +39,14 @@
                         </li>
                     @else
                         @if(!Request::is('email-confirmation/*'))
-                        <li class="nav-item">
-                            <a class="nav-link js-scroll-trigger" href="{{ route('login') }}">Login</a>
-                        </li>
-                        @if (Route::has('register'))
                             <li class="nav-item">
-                                <a class="nav-link js-scroll-trigger" href="{{ route('register') }}">Register</a>
+                                <a class="nav-link js-scroll-trigger" href="{{ route('login') }}">Login</a>
                             </li>
-                        @endif
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link js-scroll-trigger" href="{{ route('register') }}">Register</a>
+                                </li>
+                            @endif
                         @endif
                     @endauth
                 @endif
@@ -65,13 +65,13 @@
 
 
     <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ url('') }}/vendor/jquery/jquery.min.js"></script>
+    <script src="{{ url('') }}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Plugin JavaScript -->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="{{ url('') }}/vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for this template -->
-    <script src="js/grayscale.min.js"></script>
+    <script src="{{ url('') }}/js/grayscale.min.js"></script>
 </body>
 </html>
