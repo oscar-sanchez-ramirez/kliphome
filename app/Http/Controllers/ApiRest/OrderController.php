0c9ab22f41c8d7dccc2831a605ae7c8104f24048
GermanRuelasCastillo <spartperu@gmail.com>
2020-07-10 14:45:22 -0500

    Update OrderController.php

--- app/Http/Controllers/ApiRest/OrderController.php
+++ app/Http/Controllers/ApiRest/OrderController.php
@@ -199,7 +199,7 @@ class OrderController extends ApiController
                         "description" => "Payment of order ".$request->order_id
                     ]);
                 }
-                Log::pago($pago);
+                Log::notice($pago);
                 $payment = new Payment;
                 $payment->order_id = $request->order_id;
                 $payment->description = "PAGO POR SERVICIO";
