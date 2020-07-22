f56e1530d5d9fe4f5a2afe7cc33608285a649ed3
GermanRuelas <german.ruelas@tigears.com>
2020-07-22 16:33:52 -0500

    Update SocialController.php

--- app/Http/Controllers/ApiRest/SocialController.php
+++ app/Http/Controllers/ApiRest/SocialController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\ApiRest;
 
+use App\User;
 use Socialite;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
@@ -10,23 +11,49 @@ use App\Http\Controllers\ApiController;
 class SocialController extends ApiController
 {
     public function __construct(){
-        $this->middleware('auth:api', ['only' => ['gmail']]);
+        // $this->middleware('auth:api', ['only' => ['gmail','checkifexists']]);
     }
     public function facebook(Request $request) {
         Log::notice($request->all());
-        $user = Socialite::driver('facebook')->userFromToken($request->access_token);
+        try {
+            $user = Socialite::driver('facebook')->userFromToken($request->access_token);
 
-        if($user == null){
+            if($user == null){
+                return response()->json([
+                    "success" => false,
+                    "message" => 'No se encontro al usuario, inténte con otra cuenta'
+                ]);
+            }else{
+                $this->checkifexists($user);
+            }
+
             return response()->json([
-                "success" => false
+                "success" => true,
+                "token_type" => "Bearer",
+                'access_token' => $request->access_token,
+                'refresh_token' => $request->refreshToken,
+                'expired_at' => $request->expiresIn
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "success" => false,
+                "message" => 'Hubo un error inesperado, inténtelo más tarde'
             ]);
         }
 
-        return response()->json([
-            "success" => true,
-            "token_type" => "Bearer",
-            'access_token' => $request->access_token
-        ]);
+    }
+    public function checkifexists($user){
+        $check_user = User::where('email',$user->email)->first();
+        if(!$check_user){
+            $random = strtoupper(substr(md5(mt_rand()), 0, 10));
+            $user = User::create([
+                'name' => $user->name,
+                'email' => $user->email,
+                'password' => bcrypt($random),
+                'code' => $random
+            ])->toArray();
+            // dispatch(new UserConfirmation($user));
+        }
     }
     public function gmail(){
 
