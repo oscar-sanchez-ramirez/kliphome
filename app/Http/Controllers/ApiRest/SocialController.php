3308cf0ad453889ead321b0e5ce6a0ce2fa1ad31
GermanRuelas <german.ruelas@tigears.com>
2020-07-22 15:00:29 -0500

    Update SocialController.php

--- app/Http/Controllers/ApiRest/SocialController.php
+++ app/Http/Controllers/ApiRest/SocialController.php
@@ -15,7 +15,7 @@ class SocialController extends ApiController
     public function facebook(Request $request) {
         Log::notice($request->all());
         $user = Socialite::driver('facebook')->userFromToken('EAACqlQmudnUBAME6bGV56LDFkyrLigy01CWGpns6n2RZBJMb45ZCZBvEy1oR5Pq4awSjZBqZBaOT7rVjYn4HaOE5DKSPIYRK5gCihxGdPZBx00bjXr3l87VSG5VxPZAz2aZAdHHeDQwnKIQa2XIGVQWCT4TYgIEM9CmKbmGTmTZAEFXFNB9hu5NQK');
-        return $user;
+        return $user->setToken('EAACqlQmudnUBAME6bGV56LDFkyrLigy01CWGpns6n2RZBJMb45ZCZBvEy1oR5Pq4awSjZBqZBaOT7rVjYn4HaOE5DKSPIYRK5gCihxGdPZBx00bjXr3l87VSG5VxPZAz2aZAdHHeDQwnKIQa2XIGVQWCT4TYgIEM9CmKbmGTmTZAEFXFNB9hu5NQK');
         // abort_if($user == null || $user->id != $request->input('userID'),400,'Invalid credentials');
 
         // // get existing user or create new (find by facebook_id or create new record)
