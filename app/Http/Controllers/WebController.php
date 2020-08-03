f7a9b19883813a5defd178878da6cd03d56f3b17
GermanRuelas <german.ruelas@tigears.com>
2020-08-03 14:58:41 -0500

    Update WebController.php

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 use App\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use App\Jobs\Mail\UserConfirmation;
 
 class WebController extends ControllerWeb
 {
@@ -12,16 +11,14 @@ class WebController extends ControllerWeb
         return view('terminos');
     }
     public function email_verified($code){
-        $user = User::where('email','germanruelas17@gmail.com')->first();
-        dispatch(new UserConfirmation($user));
-        return "yes";
-        // if($user){
-        //     User::where('code',$code)->update([
-        //         'email_verified_at' => Carbon::now()
-        //     ]);
-        //     return view('emailverified');
-        // }else{
-        //     return view('emailnotverified');
-        // }
+        $user = User::where('code',$code)->where('email_verified_at',null)->first();
+        if($user){
+            User::where('code',$code)->update([
+                'email_verified_at' => Carbon::now()
+            ]);
+            return view('emailverified');
+        }else{
+            return view('emailnotverified');
+        }
     }
 }
