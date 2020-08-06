519c10616598e88499e60a3b988546282038074d
GermanRuelas <german.ruelas@tigears.com>
2020-08-06 14:15:47 -0500

    Update RegisterController.php

--- app/Http/Controllers/ApiRest/RegisterController.php
+++ app/Http/Controllers/ApiRest/RegisterController.php
@@ -120,15 +120,15 @@ class RegisterController extends ApiController
         $new_code->code = $number;
         $new_code->save();
 
-        Nexmo::message()->send([
-            'to'   => $num,
-            'from' => 'KlipHome',
-            'text' => $number.' es tu numero de verificacion para KlipHome',
-            'type' => 'text'
-        ]);
+        // Nexmo::message()->send([
+        //     'to'   => $num,
+        //     'from' => 'KlipHome',
+        //     'text' => $number.' es tu numero de verificacion para KlipHome',
+        //     'type' => 'text'
+        // ]);
         return response()->json([
             'success' => true,
-            'message' => "Código enviado a".$num
+            'message' => "Código enviado a".$num." es ".$number
         ]);
     }
 
