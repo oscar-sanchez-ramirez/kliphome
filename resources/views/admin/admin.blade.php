c17a39a1f6920126cd9cbc34c364959b85a31541
GermanRuelas <german.ruelas@tigears.com>
2020-08-06 18:09:07 -0500

    Update admin.blade.php

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -135,36 +135,22 @@
         }
     @endphp
     <script>
-htmlCanvas = document.getElementById('myChart'),
-        // Obtain a graphics context on the canvas element for drawing.
+        htmlCanvas = document.getElementById('myChart'),
+        myChart2 = document.getElementById('myChart2'),
         context = htmlCanvas.getContext('2d');
+        context2 = myChart2.getContext('2d');
 
-       // Start listening to resize events and draw canvas.
        initialize();
 
        function initialize() {
-           // Register an event listener to call the resizeCanvas() function
-           // each time the window is resized.
            window.addEventListener('resize', resizeCanvas, false);
-           // Draw canvas border for the first time.
            resizeCanvas();
         }
 
-        // Display custom canvas. In this case it's a blue, 5 pixel
-        // border that resizes along with the browser window.
-        function redraw() {
-           context.strokeStyle = 'blue';
-           context.lineWidth = '5';
-           context.strokeRect(0, 0, window.innerWidth, window.innerHeight);
-        }
 
-        // Runs each time the DOM window resize event fires.
-        // Resets the canvas dimensions to match window,
-        // then draws the new borders accordingly.
         function resizeCanvas() {
             htmlCanvas.width = window.innerWidth;
             htmlCanvas.height = window.innerHeight;
-            redraw();
         }
         window.onload = function() {
         var dates = @json($array_dates);
