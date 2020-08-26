60f6b799c28da02e6e011d0456b3cc9172e35e24
GermanRuelas <german.ruelas@tigears.com>
2020-08-26 10:29:33 -0500

    Update ConfigSystem.php

--- app/ConfigSystem.php
+++ app/ConfigSystem.php
@@ -9,9 +9,9 @@ class ConfigSystem extends Model
 {
     const socialmedia = [
         'success' => true,
-        'facebook_ios' => true,
+        'facebook_ios' => false,
+        'google_ios' => false,
         'facebook_android' => true,
-        'google_ios' => true,
         'google_android' => false,
     ];
 
