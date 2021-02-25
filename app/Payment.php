8ec01bd13125ed6bf99bddc7099863d020e45088
GermanRuelas <german.ruelas@tigears.com>
2021-02-25 13:37:40 -0500

    Update Payment.php

--- app/Payment.php
+++ app/Payment.php
@@ -2,19 +2,22 @@
 
 namespace App;
 
-use DB;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
 
 class Payment extends Model
 {
     public function detalle_pago($id,$price){
         $cotizacion = Quotation::where('order_id',$id)->get();
+
         $tecnico = DB::table('selected_orders as so')->join('users as u','u.id','so.user_id')->join('fixerman_stats as f','f.user_id','u.id')
         ->select('f.percent','u.name','u.lastName')->where('so.order_id',$id)->first();
         if(count($cotizacion)>1){
             foreach($cotizacion as $cot){
-                if(($cot->price + $cot->workforce) == $price){
-                    $cotizacion = $cot;
+                $visit_price = Payment::where('order_id',$id)->where('description','VISITA')->first();
+                if(((($cot->price + $cot->workforce) == $price) || (($cot->price + $cot->workforce - $visit_price->price) == $price)) && $cot->state == 1){
+                    $cotizacion = Quotation::where('id',$cot->id)->first();
                 }
             }
             return response()->json([
